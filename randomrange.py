082e27b60d84c71214aca581bfcfbdc9778b85a4
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-26 12:23:33 -0600

    Update randomrange.py

--- randomrange.py
+++ randomrange.py
@@ -10,6 +10,7 @@ import random
 #imports the random module
 
 for x in range(10):
-#assigns the loop variable to "n"    
+#assigns the loop variable to "n"
+#Set the limit to 36 so that 35 would get included in random integer range.
   print (random.randrange(25,35))
 #prints random integers between 25 and 35 to the screen
