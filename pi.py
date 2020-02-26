1f83f521192bdf679f04dfc77f98a3b104dff272
Laleh Kalantari <57807463+lkalantari@users.noreply.github.com>
2020-02-26 12:33:40 -0600

    Update pi.py

--- pi.py
+++ pi.py
@@ -16,7 +16,7 @@ for i in range(1, 10000000,4):
     pi -= 4/(i+2)
 #The subtraction operation pi -4 /1+2 (or 3)
 #Each time through the loop, it will increment i by 4 and continue
-
+#Your approach can be simlified.  You can also just use pi= 355 / 113
 print(pi)
 #executes the for loop operation and displays it to the screen
 
